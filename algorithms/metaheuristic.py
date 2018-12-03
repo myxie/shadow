77f8cb5ef966700ea29daf6039a703872e9c4b98
myxie <21298244@student.uwa.edu.au>
2018-12-03 18:10:35 +0800

    Adding some files and directory structure

---
+++ algorithms/metaheuristic.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2018 RW Bunney
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.  
+
+########################################################################
+
+# This file contains code for implementing metaheuristic scheduling algorithms.
+# Currently, this file implements the following algorithms:
+# 
+
+
+# TODO; initial setup required for a genetic algorithm
+# TODO; initial setup required for an evolutionary algorithm 
+
