c4f14146a756eb4ed723ef42b0f1abf8a07b6286
myxie <21298244@student.uwa.edu.au>
2019-08-06 20:24:50 +0800

    Generation using Ggen; conversion to json format; running tests with CP model.

---
+++ dot_json_translate.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+import networkx as nx 
+nx.drawing.nx_pydot.read_dot('dots/ggen_out_10-0.5.dot')
+testdot = nx.drawing.nx_pydot.read_dot('dots/ggen_out_10-0.5.dot')
+testdot.nodes
+testdot.nodes
+testdot.edges
+nx.readwrite.json_graph.node_link_data(testdot)
+testdot = nx.graph(nx.drawing.nx_pydot.read_dot('dots/ggen_out_10-0.5.dot'))
+testdot = nx.Graph(nx.drawing.nx_pydot.read_dot('dots/ggen_out_10-0.5.dot'))
+nx.readwrite.json_graph.node_link_data(testdot)
+testdot = nx.DiGraph(nx.drawing.nx_pydot.read_dot('dots/ggen_out_10-0.5.dot'))
+nx.readwrite.json_graph.node_link_data(testdot)
+testdot.nodes
+testdot.nodes()
+celar
+get_ipython().run_line_magic('clear', '')
+ccr = 0.1
+mean = 50
+comp_min = mean - uniform_range
+comp_max = mean + uniform_range
+mean = 50
+uniform_range = 25
+comp_min = mean - uniform_range
+comp_max = mean + uniform_range
+comp_min
+comp_max
+comm_mean = int(mean*ccr) 
+comm_mean
+comm_mean - (uniform_range * ccr) 
+comm_max = comm_mean + (uniform_range * ccr) 
+comm_max
+for node in testdot: 
+    print(node) 
+    
+for edge in testdot: 
+    print(edge)
+     
+    
+celar
+get_ipython().run_line_magic('clear', '')
+for node in testdot: 
+    testdot[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+import random 
+for node in testdot: 
+    testdot[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+for node in testdot.nodes: 
+    testdot[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+testdot[1]
+testdot['1']
+testdot
+testdot.nodes
+testdoes.nodes[1]
+testdot.nodes[1]
+testdot.node[1]
+testdot['1']['val']
+testdot['0']
+testdot.node['0']
+for node in testdot.nodes: 
+    testdot.node[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+    
+for node in testdot.nodes:
+    print(testdot.node[node]['comp'])
+    
+total = 0
+for node in testdot.nodes:
+    sum+=(testdot.node[node]['comp'])
+    
+    
+total = 0
+for node in testdot.nodes:
+    total+=(testdot.node[node]['comp'])
+    
+    
+total
+total/testdot.size()
+comp_min
+comp_max
+testdot.size()
+len(testdot)
+for node in testdot.nodes: 
+    testdot.node[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+    
+    
+total = 0
+for node in testdot.nodes:
+    total+=(testdot.node[node]['comp'])
+    
+    
+total
+comp_min
+comp_max
+testdot.nodes
+total = 0
+for node in testdot.nodes:
+    total+=(testdot.node[node]['comp'])
+    
+    
+toal
+total
+total = 0
+for node in testdot.nodes:
+    total+=(testdot.node[node]['comp'])
+    
+    
+    
+total
+for node in testdot.nodes:
+    print(testdot.node[node]['comp'])
+    
+for node in testdot.nodes: 
+    testdot.node[node]['comp'] = int(random.uniform(comp_min,comp_max))
+    
+    
+    
+    
+for node in testdot.nodes:
+    print(testdot.node[node]['comp'])
+    
+get_ipython().run_line_magic('history', '')
+get_ipython().run_line_magic('history', '> tmp.txt')
+get_ipython().run_line_magic('save', 'dot_json_translate ')
+get_ipython().run_line_magic('save', "'dot_json_translate'")
+get_ipython().run_line_magic('save', "'dot_json_translate' 1 74")
+get_ipython().run_line_magic('save', "'dot_json_translate' 1 100")
+get_ipython().run_line_magic('save', "'dot_json_translate' 1-100")
--- runner.py
+++ runner.py
@@ -11,17 +11,88 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.  
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import json
+import random
+import sys
+import os
+import subprocess
+import networkx as nx
 
 
-import argparse
+def generate_graph_costs(dot_path, ccr, mean, uniform_range, num_machines, json_path=None):
+	"""
+	:param dot_path: The path of the dot file for which we are generating cost values
+	:param ccr: Communication/Computation cost ratio
+	:param mean: The mean value for out uniform distribution
+	:param uniform_range: the range above/below the mean for the uniform distribution
+	:param num_machines: The number of machines that are in the system on which we are scheduling
+	:param json_path: If specified, use this path as output for json
+	:return: None
+	"""
 
-parser = argparse.ArgumentParser(description=\
-						"Experiment runner for experiments conducted\
-						using the MOSWS library")
+	# Use networkx to read in graph from DOT format and convert to JSON
+	dotgraph = nx.convert_node_labels_to_integers(
+		nx.DiGraph(nx.drawing.nx_pydot.read_dot(dot_path)))
 
-subparsers = parser.add_subparsers()
+	# Generate 'comp' and 'data_size' values for the graph with given CCR
+	comp_min = mean - uniform_range
+	comp_max = mean + uniform_range
+
+	comm_mean = int(mean*ccr)
+	comm_min = comm_mean - (uniform_range*ccr)
+	comm_max = comm_mean + (uniform_range*ccr)
+
+	for node in dotgraph:
+		cost_list = []
+		for p in range(num_machines):
+			cost_list.append(int(random.uniform(comp_min, comp_max)))
+		dotgraph.node[node]['comp'] = cost_list
+	for edge in dotgraph.edges:
+		dotgraph.edges[edge]['data_size'] = int(random.uniform(comm_min, comm_max))
+
+	jgraph = {'graph': nx.readwrite.node_link_data(dotgraph)}
+	# Generate place holder system values (resources/data_rate) for compatibility with shadow library format
+
+	data_rate_matrix = []
+	for x in range(num_machines):
+		row = [1 for y in range(num_machines)]
+		row[x] = 0
+		data_rate_matrix.append(row)
+
+	system = {'resource': [random.randint(0, 20) for x in range(num_machines)],
+						'data_rate': data_rate_matrix }
+	jgraph['system'] = system
+	if not json_path:
+		json_path = '{0}.json'.format(dot_path[:-4])
+	with open(json_path, 'w') as jfile:
+		json.dump(jgraph, jfile, indent=2)
 
 
-generate_parser = subparsers.add_parser(
-                'generator', help='Data generator for testing')
+if __name__ == '__main__':
+	args = sys.argv
+	print(args)
+	if args[1] == 'ggen':
+		print("Using Ggen graph generating library")
+
+		'''
+		Here, we loop through the range provided on the command line
+		'''
+		increment = 2
+		min = int(args[2])
+		max = int(args[3])
+		# prob = float(args[4])
+		# increment = int(args[5])
+
+		for x in range(min, max, increment):
+			outfile = 'dots/ggen_out_{0}-denselu.dot'.format(x)
+			print('Generating file: {0}'.format(outfile))
+			subprocess.run(['ggen', '-o', '{0}'.format(outfile), 'dataflow-graph', 'denselu', str(x)])
+
+	else:
+		print('Generating json')
+		for path in sorted(os.listdir('dots/')):
+			if 'dot' in path:
+				print('Generating json for{0}'.format(path))
+				generate_graph_costs('dots/{0}'.format(path), 1, 100, 50, 3)
+
