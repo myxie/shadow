953087f2aa15e6efceef9c1d47a0278922b981f5
myxie <21298244@student.uwa.edu.au>
2020-02-04 18:05:28 +0800

    Updated visualisation moving code into shadowgen.py

--- utils/shadowgen/shadowgen.py
+++ utils/shadowgen/shadowgen.py
@@ -19,4 +19,37 @@ shadowgen is a utility that allows us to generate different testing scripts for 
 
 shadowgen will convert, translate, and generate workflow and environemnt files
 """
+import subprocess
+import os
 
+CURR_DIR = "/home/rwb/Dropbox/PhD/writeups/observation_graph-model"
+# TARGET_DIR = '/home/rwb/Dropbox/PhD/writeups/observation_graph-model/'
+JSON_DIR = '/home/rwb/Dropbox/PhD/writeups/observation_graph-model/json/'
+
+from sample_generator import generate_graph_costs , generate_system_machines
+
+def dotgen(min,max,increment):
+	print("Using Ggen graph generating library")
+
+	'''
+	Here, we loop through the range provided on the command line
+	'''
+	# prob = float(args[4])
+	# increment = int(args[5])
+
+	for x in range(min, max, increment):
+		outfile = 'dots/ggen_out_{0}-denselu.dot'.format(x)
+		print('Generating file: {0}'.format(outfile))
+		subprocess.run(['ggen', '-o', '{0}'.format(outfile), 'dataflow-graph', 'denselu', str(x)])
+	
+def genjson():
+	for path in sorted(os.listdir(CURR_DIR)):
+		if 'dot' in path:
+			print(os.listdir(CURR_DIR))
+			print('Generating json for {0}'.format(path))
+			generate_graph_costs('{0}/{1}'.format(CURR_DIR,path),
+								 '/home/rwb/Dropbox/PhD/writeups/observation_graph-model/json/{0}.json'.format(
+									 path[:-4]), 0.5, 5000, 500, 'giga')
+			generate_system_machines(
+				'/home/rwb/Dropbox/PhD/writeups/observation_graph-model/json/{0}_sys.json'.format(path[:-4]),
+				512, 'giga', [0.9375, 0.0625], [(100, 150), (400, 500)])
