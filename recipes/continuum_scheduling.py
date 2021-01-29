445289783f51242acc2771c00bd5281694c926fb
myxie <21298244@student.uwa.edu.au>
2021-01-29 17:25:51 +0800

    continuum_scheduling.py and daliuge_shadowgen.py now updated to demonstrate the different effectiveness of heuristics when channels (i.e. the size and parallelism) of the graph increase in size (and therefore scheduling complexity).

--- recipes/continuum_scheduling.py
+++ recipes/continuum_scheduling.py
@@ -19,24 +19,67 @@ where small means 15,621 nodes. We will be scheduling this on a 'cluster'
 with 40 machines.
 """
 
+import pandas as pd
+import seaborn as sns
+
+
 from shadow.models.workflow import Workflow
 from shadow.models.environment import Environment
 from shadow.algorithms.heuristic import heft, fcfs
 
 
-WORKFLOW = "routput/shadow_Continuum_ChannelSplit.json"
+df = pd.DataFrame(columns=["time", "channels","algorithm","diff"])
+WORKFLOW = "routput/shadow_Continuum_ChannelSplit_10.json"
 CLUSTER = "routput/system_spec_40_200-400_1.0"
 
 wf_fcfs = Workflow(WORKFLOW)
 env_fcfs = Environment(CLUSTER)
 wf_fcfs.add_environment(env_fcfs)
-
-wf_heft = Workflow(WORKFLOW)
-env_heft = Environment(CLUSTER)
-wf_heft.add_environment(env_heft)
-
 soln = fcfs(wf_fcfs)
-print(soln.makespan)
+seq = -1
 
-soln2 = heft(wf_heft)
-print(soln2.makespan)
+#
+# for p in range(len(self.processors)):
+#     comp = 0
+#     for task in list(self.graph.nodes()):
+#         comp = comp + self.comp_matrix[task.tid][p]
+#     if (seq is -1) or (comp < seq):
+#         seq = comp
+
+for x in range(10, 100, 10):
+    print("Scheduling {0} Channels".format(x))
+    WORKFLOW = "routput/shadow_Continuum_ChannelSplit_{0}.json".format(x)
+    CLUSTER = "routput/system_spec_40_200-400_1.0"
+
+    wf_fcfs = Workflow(WORKFLOW)
+    env_fcfs = Environment(CLUSTER)
+    wf_fcfs.add_environment(env_fcfs)
+
+    wf_heft = Workflow(WORKFLOW)
+    env_heft = Environment(CLUSTER)
+    wf_heft.add_environment(env_heft)
+
+    soln = fcfs(wf_fcfs)
+    fcfs_res = soln.makespan
+
+    soln2 = heft(wf_heft)
+    heft_res = soln2.makespan
+    diff = abs(fcfs_res-heft_res)
+    values = {"time": fcfs_res, "channels": x, "algorithm": "fcfs",
+              "diff":diff}
+    row_to_add = pd.Series(values, name=x)
+    df = df.append(row_to_add)
+    values = {"time": heft_res, "channels": x, "algorithm": "heft",
+              "diff":diff}
+    row_to_add = pd.Series(values, name=x)
+    df = df.append(row_to_add)
+
+print(df)
+df.to_pickle("continuum_pickle.pkl")
+sns.set_style("darkgrid")
+df.time = df.time.astype(float)
+df.channels=df.channels.astype(float)
+import matplotlib.pyplot as plt
+sns.lineplot(x="channels", y="time", hue="algorithm", data=df)
+plt.show()
+plt.savefig("channels.png")
--- recipes/daliuge_shadowgen.py
+++ recipes/daliuge_shadowgen.py
@@ -27,13 +27,13 @@ logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 
 # System Reqs.
-HETEROGENEITY = [0.75,0.25]  # Homogeneous
+HETEROGENEITY = [0.75, 0.25]  # Homogeneous
 NUM_MACHINES = 40
-SPEC_RANGE = [(200,400),(800,1600)]
+SPEC_RANGE = [(200, 400), (800, 1600)]
 MAGNITUDE = 'giga'
 SYSTEM_OUTPUT_PATH = 'recipes/routput/system_spec_{0}_{1}_{2}'.format(
     NUM_MACHINES,
-    "{0}-{1}".format(SPEC_RANGE[0 ][0],SPEC_RANGE[1][1]),
+    "{0}-{1}".format(SPEC_RANGE[0][0], SPEC_RANGE[1][1]),
     "{0}-{1}".format(HETEROGENEITY[0], HETEROGENEITY[1])
 )
 
@@ -56,75 +56,75 @@ logger.info(system_config_path)
 # Generating graph costs
 unrolled_graph_json = daliuge.unroll_logical_graph(LOGICAL_GRAPH)
 
-graph_list = []
 
-CHANNELS = 2
-for i in range(0, CHANNELS):
-    identifier = "c{0}_".format(i)
-    nxgraph,output_graph_path = daliuge.json_to_shadow(
-        unrolled_graph_json, SHADOW_OUTPUT, MEAN, UNIFORM_RANGE, MULTIPLIER,
-        CCR,node_identifier=identifier,
-        seed=20, data_intensive=True
-    )
-    graph_list.append(nxgraph)
+for x in range(10,110,10):
+    CHANNELS = x
+    graph_list = []
+    for i in range(0, CHANNELS):
+        identifier = "c{0}_".format(i)
+        nxgraph, output_graph_path = daliuge.json_to_shadow(
+            unrolled_graph_json, SHADOW_OUTPUT, MEAN, UNIFORM_RANGE, MULTIPLIER,
+            CCR, node_identifier=identifier,
+            seed=20, data_intensive=True
+        )
+        graph_list.append(nxgraph)
+        print(nxgraph.nodes())
 
-head = 'channel_split'
-channel_graph = nx.DiGraph()
-children = [head+str(i) for i in range(0,CHANNELS)]
-channel_graph.add_nodes_from(children)
-channel_graph.add_node(head)
-channel_graph.add_edges_from([(head, head+str(x)) for x in range(0, CHANNELS)])
-for node in channel_graph.nodes():
-    channel_graph.nodes[node]['comp'] = 100000
-for edge in channel_graph.edges():
-    channel_graph.edges[edge]['data_size']=0
-i = 0
-# glist = []
-# for x in range(0,len(children)):
-#     ng = nx.DiGraph()
-#     ng.add_nodes_from([z for z in range(i, i + len(children))])
-#     ng.add_edges_from([(z, z + 1) for z in range(i, i+len(children)-1)])
-#     glist.append(ng)
-#     i += len(children)
+    head = 'channel_split'
+    channel_graph = nx.DiGraph()
+    children = [head + str(i) for i in range(0, CHANNELS)]
+    channel_graph.add_nodes_from(children)
+    channel_graph.add_node(head)
+    channel_graph.add_edges_from(
+        [(head, head + str(x)) for x in range(0, CHANNELS)])
+    for node in channel_graph.nodes():
+        channel_graph.nodes[node]['comp'] = 100000
+    for edge in channel_graph.edges():
+        channel_graph.edges[edge]['data_size'] = 0
+    # glist = []
+    # for x in range(0,len(children)):
+    #     ng = nx.DiGraph()
+    #     ng.add_nodes_from([z for z in range(i, i + len(children))])
+    #     ng.add_edges_from([(z, z + 1) for z in range(i, i+len(children)-1)])
+    #     glist.append(ng)
+    #     i += len(children)
 
-glistgraph = nx.compose_all(graph_list)
-final = nx.compose(glistgraph, channel_graph)
+    glistgraph = nx.compose_all(graph_list)
+    final = nx.compose(glistgraph, channel_graph)
 
-for i in range(0,CHANNELS):
-    minor_heads = []
-    for node in graph_list[i].nodes():
-        pred = list(final.predecessors(node))
-        if len(pred) == 0:
-            minor_heads.append(node)
-    for node in minor_heads:
-        final.add_edge(head+str(i),node,data_size=0)
+    for i in range(0, CHANNELS):
+        minor_heads = []
+        for node in graph_list[i].nodes():
+            pred = list(final.predecessors(node))
+            if len(pred) == 0:
+                minor_heads.append(node)
+        for node in minor_heads:
+            final.add_edge(head + str(i), node, data_size=0)
 
-nx.drawing.nx_pydot.write_dot(final, 'testgraph.dot')
+    nx.drawing.nx_pydot.write_dot(final, 'testgraph.dot')
 
-jgraph = {
-    "header": {
-        "time": False,
-        "gen_specs": {
-            'file': "channel_split_continuum.json",
-            'mean': MEAN,
-            'range': "+-{0}".format(UNIFORM_RANGE),
-            'seed': 20,
-            'ccr': CCR,
-            'multiplier': MULTIPLIER
+    jgraph = {
+        "header": {
+            "time": False,
+            "gen_specs": {
+                'file': "channel_split_continuum.json",
+                'mean': MEAN,
+                'range': "+-{0}".format(UNIFORM_RANGE),
+                'seed': 20,
+                'ccr': CCR,
+                'multiplier': MULTIPLIER
+            },
         },
-    },
-    'graph': nx.readwrite.node_link_data(final)
-}
+        'graph': nx.readwrite.node_link_data(final)
+    }
 
-with open(
-        "{0}".format(
-            "recipes/routput/shadow_Continuum_ChannelSplit.json"
-        ),'w'
-) as jfile:
-    json.dump(jgraph, jfile, indent=2)
+    with open(
+            "recipes/routput/shadow_Continuum_ChannelSplit_{0}.json".format(
+                CHANNELS)
+            , 'w'
+    ) as jfile:
+        json.dump(jgraph, jfile, indent=2)
 
+    daliuge.generate_dot_from_networkx_graph(final, 'output')
 
-daliuge.generate_dot_from_networkx_graph(final,'output')
-
-logger.info(output_graph_path)
-
+    logger.info(output_graph_path)
